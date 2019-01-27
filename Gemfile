d0f38026a565f09d444c722e7e9443e032a2363e
shafrazi <shafrazi@live.com>
2019-01-27 15:06:51 +0530

    add controllers and models for categories and images

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem "bootstrap"
 gem 'jquery-rails'
+gem "rmagick"
+gem "carrierwave"
+gem "autoprefixer-rails"
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  
+
+  get 'images/new'
+  get "upload", to: "images#new"
+  get 'images/edit'
+  get 'images/update'
+  get 'images/destroy'
+  get 'images/index'
+  get 'images/show'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/destroy'
+  get 'categories/show'
+
+  resources :images, :categories
+
+  root "images#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
